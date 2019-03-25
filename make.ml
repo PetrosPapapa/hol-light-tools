c28ede2f9e16d8840af188d9eabb5712244d5e23
Petros Papapanagiotou <petrospapapan@gmail.com>
2019-03-25 16:53:12 +0000

    Updates make.ml (minor)

--- make.ml
+++ make.ml
@@ -4,7 +4,7 @@
 (*                                                                           *)
 (*                           Petros Papapanagiotou                           *)
 (*                          University of Edinburgh                          *)
-(*                                2010 - 2016                                *)
+(*                                2010 - 2019                                *)
 (* ========================================================================= *)
 
 loads ("tools/lib.ml");;
@@ -12,6 +12,5 @@ loads ("tools/vars.ml");;
 loads ("tools/terms.ml");;
 loads ("tools/tactics.ml");;
 loads ("tools/rules.ml");;
-loads ("tools/proofcounter.ml");;
 loads ("tools/proofcounter.ml");;
 loads ("tools/etactics.ml");;
